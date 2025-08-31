ed0ce69525fee5740b9aae20cabd90374d211371
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-08-31 19:59:08 -0400

    c

--- example_repo/bin/g5_t2_s1_publish_package.py
+++ example_repo/bin/g5_t2_s1_publish_package.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 """
-We primarily use twine to publish the package for open source project.
+We primarily use twine to publish the package to AWS CodeArtifact.
 """
 
 from pywf import pywf
 
-pywf.twine_upload()
+pywf.publish_to_codeartifact(real_run=True, verbose=True)
